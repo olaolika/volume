bf6d358eefdee59b99002eda18704a89a2d6509a
olaolika <olika.januszkiewicz@gmail.com>
2022-11-27 19:06:59 +0200

    русские надписи

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,12 @@
-fun main(args: Array<String>) {
+fun main() {
     //калькулятор объема параллелепипеда
-    println("Calculate the volume of the parallelepiped\n" +
-            "Enter length")
+    println("Вычислить объем параллелепипеда\n" +
+            "введите значение длины в см")
     val length = readln().toInt()
-    println("enter width")
+    println("введите значение ширины в см")
     val width = readln().toInt()
-    println("enter height")
+    println("введите значение высоты в см")
     val height = readln().toInt()
-
     val volume = length * width * height
-    println("Volume of the parallelepiped")
-    println(volume)
+    println("объем равен $volume кубических сантиметра")
 }
